5a488f57409ccd798dec8ef02b7eb9da9fbdb852
Matthias Beyer <mail@beyermatthias.de>
2015-02-04 18:19:33 +0100

    Add nix-script.sh implementation (first lines)

--- nix-script.sh
+++ nix-script.sh
@@ -10,3 +10,47 @@
 # So the "diff-generations" script looks like a command for "nix-script"
 #
 
+VERBOSE=0
+CONFIGFILE=~/.nixscriptsrc
+
+cmd="$1"; shift
+
+case $cmd in
+"--list-commands" )
+    LIST_COMMANDS=1
+    shift;
+    ;;
+
+"-l" )
+    LIST_COMMANDS=1
+    shift;
+    ;;
+
+"--config" )
+    CONFIGFILE=$1
+    shift
+    ;;
+
+"-c" )
+    CONFIGFILE=$1
+    shift
+    ;;
+
+"-v" )
+    export VERBOSE=1
+    shift;
+    ;;
+esac
+
+[[ ! -f $CONFIGFILE ]] && echo "No config file: '$CONFIGFILE'" && exit 1
+
+if [[ $LIST_COMMNADS -eq 1 ]]
+then
+    for cmd in $(all_commands)
+    do
+        echo "$cmd"
+    done
+    exit 0
+fi
+
+exec nixos-scripts-$cmd $*
